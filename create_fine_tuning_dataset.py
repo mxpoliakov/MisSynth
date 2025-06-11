2a88b77e1a42d42060068ffd7f64d999b671e42e
Mykhailo Poliakov <mxpoliakov@gmail.com>
2025-06-11 11:34:39 +0300

    Add gibberish baseline option

--- create_fine_tuning_dataset.py
+++ create_fine_tuning_dataset.py
@@ -2,6 +2,7 @@ import os
 import re
 from copy import copy
 
+import lorem
 import typer
 
 from common import DEFAULT_SYSTEM_PROMPT
@@ -45,8 +46,12 @@ def get_raw_output_str(raw_output_folder: str, sample_id: str) -> str | None:
         return None
 
 
+def generate_lorem_ipsum() -> str:
+    return lorem.get_sentence(count=(1, 5))
+
+
 def add_synthetic_fallacies_prompts(
-    train_dataset_list: list[dict], raw_output_folder: str, prompt_template: str, sample: dict
+    train_dataset_list: list[dict], raw_output_folder: str, prompt_template: str, sample: dict, random_baseline: bool
 ) -> None:
     additional_fallacy_mapping_dict = get_additional_fallacy_mapping_dict()
     valid_classes = get_valid_fallacy_names()
@@ -60,7 +65,7 @@ def add_synthetic_fallacies_prompts(
         if not synthetic_class:
             continue
 
-        fallacy_text = match[1].strip()
+        fallacy_text = match[1].strip() if not random_baseline else generate_lorem_ipsum()
 
         synthetic_class = additional_fallacy_mapping_dict.get(synthetic_class, synthetic_class)
         synthetic_class = normalize_fallacy_name(synthetic_class, fail_if_unk_fallacy=False)
@@ -81,7 +86,7 @@ def add_synthetic_fallacies_prompts(
 
 
 def add_synthetic_claim_premise_prompts(
-    train_dataset_list: list[dict], raw_output_folder: str, prompt_template: str, sample: dict
+    train_dataset_list: list[dict], raw_output_folder: str, prompt_template: str, sample: dict, random_baseline: bool
 ) -> None:
     pattern = re.compile(
         r"""(?mx)
@@ -104,12 +109,14 @@ def add_synthetic_claim_premise_prompts(
     for fallacy in sample["argument"]["fallacies"]:
         for interchangeable_fallacy in fallacy["interchangeable_fallacies"]:
             for match in matches:
+                claim = match[1].strip() if not random_baseline else generate_lorem_ipsum()
+                premise = match[2].strip() if not random_baseline else generate_lorem_ipsum()
                 train_dataset_list.append(
                     {
                         "prompt": get_prompt(
                             prompt_template,
-                            claim=match[1].strip(),
-                            premise=match[2].strip(),
+                            claim=claim,
+                            premise=premise,
                             fallacy_text=interchangeable_fallacy["premise"],
                         ),
                         "completion": f"Fallacy: {interchangeable_fallacy['class']}",
@@ -121,6 +128,7 @@ def create_fine_tuning_dataset(
     prompt_template: str = "cls_without_premise/p4-connect-cls-D.txt",
     split: MissciSplit = MissciSplit.TEST,
     raw_output_folders: list[str] | None = None,
+    random_baseline: bool = False,
 ) -> None:
     if raw_output_folders is None:
         raw_output_folders = [output_file for output_file in os.listdir("output") if ".jsonl" not in output_file]
@@ -133,9 +141,13 @@ def create_fine_tuning_dataset(
     for sample in data:
         for raw_output_folder in raw_output_folders:
             if "single-class-synthetic-fallacy" in raw_output_folder:
-                add_synthetic_fallacies_prompts(train_dataset_list, raw_output_folder, prompt_template, sample)
+                add_synthetic_fallacies_prompts(
+                    train_dataset_list, raw_output_folder, prompt_template, sample, random_baseline
+                )
             elif "synthetic-claim-premise" in raw_output_folder:
-                add_synthetic_claim_premise_prompts(train_dataset_list, raw_output_folder, prompt_template, sample)
+                add_synthetic_claim_premise_prompts(
+                    train_dataset_list, raw_output_folder, prompt_template, sample, random_baseline
+                )
             else:
                 message = f"{raw_output_folder} has no valid parser functions"
                 raise ValueError(message)
